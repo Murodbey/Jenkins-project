ec29cc587bbf79573ae513851973c05ba84a76ea
Murodbey <abdullayevmurod@gmail.com>
2019-04-20 17:30:21 -0500

    updated

--- vaultDeployment/Jenkinsfile.groovy
+++ vaultDeployment/Jenkinsfile.groovy
@@ -1,14 +1,13 @@
     
 node {
   properties([parameters([string(defaultValue: 'plan', description: 'Please provide what action you want? (plan,apply,destroy)', name: 'terraformPlan', trim: true), string(defaultValue: 'default_token_add_here', description: 'Please provide a token for vault', name: 'Provide token', trim: true)])])
-  stages{
-    stage("Git Pull"){
-            steps{
-                git 'https://github.com/Murodbey/Jenkins-project.git'
-            }
-        }
-    stage("Terraform init"){ 
-      dir("${workspace}/vaultDeployment") {
+  // stages{
+  //   stage("Git Pull"){
+  //           steps{
+  //               git 'https://github.com/Murodbey/Jenkins-project.git'
+  //           }
+  //       }
+    stage("Terraform init") {
         sh 'ls'
         sh 'pwd'
         sh "terraform init"
