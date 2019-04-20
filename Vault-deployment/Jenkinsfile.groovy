a38db0940fabee22504cf31e31edff773d69d7ed
Murodbey <abdullayevmurod@gmail.com>
2019-04-19 20:04:59 -0500

    Deployment created

---
+++ Vault-deployment/Jenkinsfile.groovy
@@ -0,0 +1,16 @@
+    
+node {
+  properties([parameters([string(defaultValue: 'plan', description: 'Please provide what action you want? (plan,apply,destroy)', name: 'Terraform_plan_apply_destroy', trim: true), string(defaultValue: 'default_token_add_here', description: 'Please provide a token for vault', name: 'Provide token', trim: true)])])
+      stage("Terraform init"){
+        sh "terraform init"
+    }
+    stage("Terraform Plan/Apply/Destroy"){
+      if (params.terraformPlan) {
+        sh "terraform plan --auto-approve"
+      }
+    } if (params.terraformApply) {
+        sh "terraform apply --auto-approve"
+    } if (params.terraformDestroy) {
+        sh "terraform destroy --auto-approve"
+    }
+}
