c75714b618806b580989f54d3df9ef93580c637e
Murodbey <abdullayevmurod@gmail.com>
2019-04-20 18:50:06 -0500

    updated

--- vaultDeployment/Jenkinsfile.groovy
+++ vaultDeployment/Jenkinsfile.groovy
@@ -1,27 +1,31 @@
     
 node('master') {
-  properties([parameters([string(defaultValue: 'plan', description: 'Please provide what action you want? (plan,apply,destroy)', name: 'terraformPlan', trim: true), string(defaultValue: 'default_token_add_here', description: 'Please provide a token for vault', name: 'Provide token', trim: true)])])
+  properties([parameters([
+    string(defaultValue: 'plan', description: 'Please provide what action you want? (plan,apply,destroy)', name: 'terraformPlan', trim: true), 
+    string(defaultValue: 'default_token_add_here', description: 'Please provide a token for vault', name: 'Provide token', trim: true)
+    ]
+    )])
+    checkout scm
     stage("Terraform init") {
-      ws("${workspace}/deployment/vault/") {
-        sh 'cd ${workspace}'
+      dir("${workspace}/deployment/vault/") {
         sh 'ls'
         sh 'pwd'
         sh "terraform init"
       }
     stage("Terraform Plan/Apply/Destroy"){
       if (params.terraformPlan.toLowerCase() == 'plan') {
-        ws("${workspace}/deployment/vault/") {
+        dir("${workspace}/deployment/vault/") {
           sh "terraform plan --auto-approve"
         }
       } 
       if (params.terraformPlan.toLowerCase() == 'apply') {
-          ws("${workspace}/deployment/vault/") {
+          dir("${workspace}/deployment/vault/") {
             sh "terraform apply --auto-approve"
           }
         } 
 
       if (params.terraformPlan.toLowerCase() == 'destroy') {
-         ws("${workspace}/deployment/vault/") {
+         dir("${workspace}/deployment/vault/") {
             sh "terraform destroy --auto-approve"
           }
       }
