ab9246b05015c600ad242f4d4799904ef4bfa248
Murodbey <abdullayevmurod@gmail.com>
2019-04-20 17:52:11 -0500

    updated

--- vaultDeployment/Jenkinsfile.groovy
+++ vaultDeployment/Jenkinsfile.groovy
@@ -1,5 +1,5 @@
     
-node {
+node('master') {
   properties([parameters([string(defaultValue: 'plan', description: 'Please provide what action you want? (plan,apply,destroy)', name: 'terraformPlan', trim: true), string(defaultValue: 'default_token_add_here', description: 'Please provide a token for vault', name: 'Provide token', trim: true)])])
   // stages{
   //   stage("Git Pull"){
