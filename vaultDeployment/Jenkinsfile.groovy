ea335028161b03d8b34e2ffb66c94c4693f8b756
Murodbey <abdullayevmurod@gmail.com>
2019-04-20 18:20:43 -0500

    updated

--- vaultDeployment/Jenkinsfile.groovy
+++ vaultDeployment/Jenkinsfile.groovy
@@ -2,25 +2,25 @@
 node('master') {
   properties([parameters([string(defaultValue: 'plan', description: 'Please provide what action you want? (plan,apply,destroy)', name: 'terraformPlan', trim: true), string(defaultValue: 'default_token_add_here', description: 'Please provide a token for vault', name: 'Provide token', trim: true)])])
     stage("Terraform init") {
-      dir("${workspace}/deployment/vault") {
+      dir("${workspace}") {
         sh 'ls'
         sh 'pwd'
         sh "terraform init"
       }
     stage("Terraform Plan/Apply/Destroy"){
       if (params.terraformPlan.toLowerCase() == 'plan') {
-        dir("${workspace}/deployment/vault") {
+        dir("${workspace}") {
           sh "terraform plan --auto-approve"
         }
       } 
       if (params.terraformPlan.toLowerCase() == 'apply') {
-          dir("${workspace}/deployment/vault") {
+          dir("${workspace}") {
             sh "terraform apply --auto-approve"
           }
         } 
 
       if (params.terraformPlan.toLowerCase() == 'destroy') {
-         dir("${workspace}/deployment/vault") {
+         dir("${workspace}") {
             sh "terraform destroy --auto-approve"
           }
       }
