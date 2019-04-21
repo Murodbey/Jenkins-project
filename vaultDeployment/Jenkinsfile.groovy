1aa7b5c0d593235f2108ce2b93e795d7ce8a1815
Farkhod <sadykovfarkhod@gmail.com>
2019-04-21 13:10:51 -0500

    Checking for file exist

--- vaultDeployment/Jenkinsfile.groovy
+++ vaultDeployment/Jenkinsfile.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurper
 
 node('master') {
   properties([parameters([
-    string(defaultValue: 'plan', description: 'Please provide what action you want? (plan,apply,destroy)', name: 'terraformPlan', trim: true), 
+    string(defaultValue: 'plan', description: 'Please provide what action you want? (plan,apply,destroy)', name: 'terraformPlan', trim: true),
     string(defaultValue: 'default_token_add_here', description: 'Please provide a token for vault', name: 'vault_token', trim: true)
     ]
     )])
@@ -13,7 +13,10 @@ node('master') {
         def file = new File("${WORKSPACE}/vaultDeployment/vault.tfvars")
         file.write """
         vault_token              =  "${vault_token}"
+
         """
+        sh "ls ${WORKSPACE}/vaultDeployment/"
+        sh "ls ${WORKSPACE}/vaultDeployment/vault.tfvars"
       }
     stage("Terraform init") {
       dir("${workspace}/vaultDeployment/") {
@@ -26,12 +29,12 @@ node('master') {
         dir("${workspace}/vaultDeployment/") {
           sh "terraform plan -var-file=variables.tfvars"
         }
-      } 
+      }
       if (params.terraformPlan.toLowerCase() == 'apply') {
           dir("${workspace}/vaultDeployment/") {
             sh "terraform apply --auto-approve"
           }
-        } 
+        }
 
       if (params.terraformPlan.toLowerCase() == 'destroy') {
          dir("${workspace}/vaultDeployment/") {
