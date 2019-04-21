78bb4b7276bb447d331bdeb2a9b37d594a293433
Murodbey <abdullayevmurod@gmail.com>
2019-04-21 16:42:34 -0500

    Updated

---
+++ vaultDeployment/booleanparams2/Jenkinsfile.groovy
@@ -0,0 +1,37 @@
+#!/usr/bin/env groovy
+package com.lib
+import groovy.json.JsonSlurper
+
+node('master') {
+  properties([parameters([
+    booleanParam(defaultValue: false, description: 'Apply All Changes', name: 'terraformApply'),  
+    string(defaultValue: 'default_token', description: 'Please provide a token for vault', name: 'vault_token', trim: true)
+    ]
+    )])
+    checkout scm
+    stage('Generate Vars') {
+        def file = new File("${WORKSPACE}/vaultDeployment/vault.tfvars")
+        file.write """
+        vault_token              =  "${vault_token}"
+
+        """
+      }
+    stage("Terraform init") {
+      dir("${workspace}/vaultDeployment/") {
+        sh "terraform init"
+      }
+    }
+    stage('Terraform Apply/Plan') {
+      if (params.terraformApply) {
+        dir("${WORKSPACE}/vaultDeployment/") {
+          echo "##### Terraform Applying the Changes ####"
+          sh "terraform apply  --auto-approve  -var-file=vault.tfvars"
+        }
+    } else {
+        dir("${WORKSPACE}/vaultDeployment/") {
+          echo "##### Terraform Plan (Check) the Changes ####"
+          sh "terraform plan -var-file=vault.tfvars"
+        }
+      } 
+    }
+}
