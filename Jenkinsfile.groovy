f965926bdc5508a692aef15bfdbaa11150f17fa8
Murodbey <abdullayevmurod@gmail.com>
2019-04-24 03:12:09 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ node('master') {
         echo "##### Creating tfvars file ####"
         def file = new File("${env.WORKSPACE}/Vault-deployment/vault.tfvars")
         file.write """
-        secret              =  "${vault-token}"
+        secret              =  "${secret}"
         namespace           =  "${namespace}"
         """
       }
