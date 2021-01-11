9f6a0708a29f51e97de18d71caaac0d133abbe40
Murodbey <abdullayevmurod@gmail.com>
2021-01-11 10:31:23 -0600

    terraform vpc

--- terraform-vpc.groovy
+++ terraform-vpc.groovy
@@ -1,3 +1,4 @@
+/* groovylint-disable CompileStatic, CouldBeSwitchStatement, DuplicateStringLiteral, LineLength, NoDef, VariableName, VariableTypeRequired */
 properties([
     parameters([
         booleanParam(defaultValue: true, description: 'Do you want to run terrform apply', name: 'terraform_apply'),
@@ -8,52 +9,52 @@ properties([
 
 def aws_region_var = ''
 
-if(params.environment == "dev"){
-    aws_region_var = "us-east-1"
+if (params.environment == 'dev') {
+    aws_region_var = 'us-east-1'
 }
-else if(params.environment == "qa"){
-    aws_region_var = "us-east-2"
+else if (params.environment == 'qa') {
+    aws_region_var = 'us-east-2'
 }
-else if(params.environment == "prod"){
-    aws_region_var = "us-west-2"
+else if (params.environment == 'prod') {
+    aws_region_var = 'us-west-2'
 }
 
-node{
-    stage("Pull Repo"){
+node {
+    stage('Pull Repo') {
         cleanWs()
         git url: 'https://github.com/nargiza777/terraform-vpc.git'
     }
 
     withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
         withEnv(["AWS_REGION=${aws_region_var}"]) {
-            stage("Terrraform Init"){
+            stage('Terrraform Init') {
                 sh """
                     bash setenv.sh ${environment}.tfvars
                     terraform-0.13 init
                 """
-            }        
-            
+            }
+
             if (terraform_apply.toBoolean()) {
-                stage("Terraform Apply"){
+                stage('Terraform Apply') {
                     sh """
                         terraform-0.13 apply -var-file ${environment}.tfvars -auto-approve
                     """
                 }
             }
             else if (terraform_destroy.toBoolean()) {
-                stage("Terraform Destroy"){
+                stage('Terraform Destroy') {
                     sh """
                         terraform-0.13 destroy -var-file ${environment}.tfvars -auto-approve
                     """
                 }
             }
             else {
-                stage("Terraform Plan"){
+                stage('Terraform Plan') {
                     sh """
                         terraform-0.13 plan -var-file ${environment}.tfvars
                     """
                 }
             }
-        }        
-    }    
+        }
+    }
 }
